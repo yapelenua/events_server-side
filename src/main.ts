d0aaad40d3edbe49c55f458fafbdc68e9a783319
yapelenua <kbshnik2020@gmail.com>
2024-05-27 22:09:22 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AllExceptionsFilter } from './filters/all-exceptions.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
+
+    app.enableCors({
+      origin: false,
+    });
+  
   await app.listen(8080);
 }
 bootstrap();
